69575de12c38bb5c22fb61f674341ee4fb53154e
bitbrain <822035+bitbrain@users.noreply.github.com>
2024-11-09 13:38:58 +0000

    Improve spawning of pads

--- godot/spawn/object_spawner.gd
+++ godot/spawn/object_spawner.gd
@@ -1,30 +1,35 @@
 extends Node2D
 
 
-const Pad = preload("res://pad/pad.tscn")
-
-
-@export var interval:float = 2.0:
-	set(i):
-		interval = i
-		if timer:
-			timer.wait_time = interval
+@export var min_interval:float = 2.0
+@export var max_interval:float = 5.0
 @export var spawn_offset:Vector2
+@export var spawn_limit = 3
+@export var scene:PackedScene
 
 
 @onready var timer: Timer = $Timer
 @onready var objects = get_tree().get_first_node_in_group("Objects")
 
 
+var current_spawn_count = 0
+
+
 func _ready() -> void:
-	timer.wait_time = interval
+	randomize()
+	timer.wait_time = min_interval + randf_range(0.0, max_interval - min_interval)
 	timer.timeout.connect(_on_timeout)
 	
 
 func _on_timeout() -> void:
-	var pad = Pad.instantiate()
-	objects.add_child(pad)
+	timer.wait_time = min_interval + randf_range(0.0, max_interval - min_interval)
+	if current_spawn_count == spawn_limit:
+		return
+	var scene = scene.instantiate()
+	objects.add_child(scene)
 	
-	pad.position.x = randf_range(-100, 100)
-	pad.global_position = global_position + spawn_offset
+	scene.position.x = randf_range(-100, 100)
+	scene.global_position = global_position + spawn_offset
+	current_spawn_count += 1
+	scene.tree_exiting.connect(func(): current_spawn_count -= 1)
 	
